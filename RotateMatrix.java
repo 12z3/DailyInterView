a48ec81440893ed80331c6c9acfbea4665da8c7e
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-03 18:17:03 +0300

    Add files via upload

---
+++ RotateMatrix.java
@@ -0,0 +1,31 @@
+package DailyInterView;
+
+import training.Methods;
+
+public class RotateMatrix extends Methods {
+    public static void main(String[] args) {
+
+        /**
+         * @Source: This problem was recently asked by Uber:
+                    Given a square 2D matrix (n x n), rotate the matrix by 90 degrees clockwise.
+         */
+
+        int[][] matrix = {{1,2,3},
+                          {4,5,6},
+                          {7,8,9}};
+
+        int[][] result = {{7,4,1},
+                          {8,5,2},
+                          {9,6,3}};
+
+        int[][] tmp = new int[3][3];
+
+        for (int col = matrix.length - 1; col >= 0; col--) {
+            for (int row = 0; row < matrix.length; row++) {
+            tmp[row][matrix.length - 1 - col] = matrix[col][row];
+            }
+        }
+
+        print2DArray(tmp);
+    }
+}
