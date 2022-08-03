b3284d3329ff304a321be8dcd67aec5626eeb4df
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-03 21:55:19 +0300

    Add files via upload

--- PythagoreanTriplet.java
+++ PythagoreanTriplet.java
@@ -3,24 +3,12 @@ package DailyInterView;
 import training.Methods;
 
 public class PythagoreanTriplet extends Methods {
-    public static void main(String[] args) {
-
-
-        /**
-         @Source: https://www.youtube.com/watch?v=18wOrbRN5co
-         -> Pythagorean triplet; a, b, c -> (aˆ2 ?= bˆ2 + cˆ2); Uber.
-         */
-
-        int[] input = {3, 5, 12, 5, 13};
-
-        pythagoreanTriplet(input);
-    }
 
     private static void pythagoreanTriplet(int[] input) {
+        int a, b, c;
         StringBuilder stb = new StringBuilder();
         for (int i = 0; i < input.length - 1; i++) {
-            int a = input[i];
-            int b, c;
+            a = input[i];
             for (int j = i + 1; j < input.length - 1; j++) {
                 b = input[j];
                 if (input[i] != input[j]) {
@@ -35,5 +23,17 @@ public class PythagoreanTriplet extends Methods {
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        /**
+         @Source: https://www.youtube.com/watch?v=18wOrbRN5co
+         -> Pythagorean triplet; a, b, c -> (aˆ2 ?= bˆ2 + cˆ2); Uber.
+         */
+
+        int[] input = {3, 5, 12, 5, 13};
+
+        pythagoreanTriplet(input);
     }
 }
