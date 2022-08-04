790c21e14bd66b1d0a8b8c8c1699904f37af4917
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-04 20:51:35 +0300

    Add files via upload

--- MicrosoftTask.java
+++ MicrosoftTask.java
@@ -6,9 +6,7 @@ public class MicrosoftTask {
         int max = Integer.MIN_VALUE;
         int a, b, c, result, tmp = 0;
 
-        for (int i = 0; i < input.length; i++) {
-            array[i] = Math.abs(input[i]);
-        }
+        for (int i = 0; i < input.length; i++) array[i] = Math.abs(input[i]);
 
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
@@ -29,14 +27,14 @@ public class MicrosoftTask {
         result = a * b * c;
         System.out.printf("\nResult = %d * %d * %d = %d", a, b, c, result);
     }
+
     public static void main(String[] args) {
 
-        /** @Problem:
-         Hi, here's your problem today. This problem was recently asked by Microsoft:
-         You are given an array of integers.
-         Return the largest product that can be made by multiplying any 3 integers in the array.
-         Example:
-         [-4, -4, 2, 8] should return 128 as the largest product can be made by multiplying -4 * -4 * 8 = 128.*/
+        /** @Problem: Hi, here's your problem today. This problem was recently asked by Microsoft:
+        You are given an array of integers.
+        Return the largest product that can be made by multiplying any 3 integers in the array.
+        Example:
+        [-4, -4, 2, 8] should return 128 as the largest product can be made by multiplying -4 * -4 * 8 = 128.*/
 
         int[] input = {-4, -4, 2, 8};
         resolvedProblem(input);
