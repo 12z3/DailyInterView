287c19e8d091dc3c9c298e9eb2ff70a6c857ab0b
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-06 07:44:41 +0300

    Add files via upload

--- Matrix2DManipulating.java
+++ Matrix2DManipulating.java
@@ -27,9 +27,9 @@ public class Matrix2DManipulating extends Methods {
         print2DArray(tmp);
         System.out.println();
 
-        for (int col = 0; col < matrix.length ; col++) {                             // 1, 4, 7   // 1, 2, 3
-            for (int row = 0; row < matrix[col].length; row++) {                     // 2, 5,8    // 4, 5, 6
-                tmp[row][col] = matrix[col][row];                                    // 3, 6, 9   // 7, 8, 9
+        for (int col = 0; col < matrix.length ; col++) {                             // 1, 2, 3   // 1, 2, 3
+            for (int row = 0; row < matrix[col].length; row++) {                     // 4, 5, 6   // 4, 5, 6
+                tmp[col][row] = matrix[col][row];                                    // 7, 8, 9   // 7, 8, 9
 
             }
         }
@@ -38,24 +38,7 @@ public class Matrix2DManipulating extends Methods {
 
         // ------------------------------------------------------------------------------------------------------------
 
-        for (int col = 0; col < matrix.length ; col++) {                              // 1, 4, 7   // 1, 2, 3
-            for (int row = 0; row < matrix[col].length; row++) {                      // 2, 5, 8   // 4, 5, 6
-                tmp[row][col] = matrix[col][row];                                     // 3, 6, 9   // 7, 8, 9
-
-            }
-        }
-        print2DArray(tmp);
-        System.out.println();
-
-        for (int col = 0; col < matrix.length ; col++) {                              // 1, 2, 3   // 1, 2, 3
-            for (int row = 0; row < matrix[col].length; row++) {                      // 4, 5, 6   // 4, 5, 6
-                tmp[col][row] = matrix[col][row];                                     // 7, 8, 9   // 7, 8, 9
-
-            }
-        }
-        print2DArray(tmp);
-        System.out.println();
-
+        
         // ------------------------------------------------------------------------------------------------------------
 
     }
