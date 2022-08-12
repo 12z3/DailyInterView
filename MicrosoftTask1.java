0e408bba1fa77ea19b0976e2e80c606c373419eb
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-12 16:20:08 +0300

    Add files via upload

--- MicrosoftTask1.java
+++ MicrosoftTask1.java
@@ -8,6 +8,8 @@ public class MicrosoftTask1 extends Methods {
         /**
          @problem: You are given an array of integers.
          Return the largest product that can be made by multiplying any 3 integers in the array.
+         Example:
+         [-4, -4, 2, 8] should return 128 as the largest product can be made by multiplying -4 * -4 * 8 = 128.
          */
 
         int[] input = {-4, -4, 2, 8};
@@ -15,8 +17,8 @@ public class MicrosoftTask1 extends Methods {
         StringBuilder resStb = new StringBuilder();
         int res = 0, old = 0;
 
-        for (int i : input) {                   // Ред: 20 -> изключва текущият елемент от сметката.
-            for (int l = 0, k = 0; l < input.length - 1; l++, k++) {
+        for (int i : input) {                   // Ред: 22 -> изключва текущият елемент от сметката.
+            for (int l = 0; l < input.length - 1; l++) {
                 if (i != input[l]) res = Math.abs(i) * Math.abs(input[l] * Math.abs(input[l + 1]));
                 if (res > max) {
                     max = res;
@@ -25,6 +27,6 @@ public class MicrosoftTask1 extends Methods {
                 }
             }
         }
-        System.out.printf("%d -> %s ", max, resStb);
+        System.out.printf("%s = %d ", resStb, max);
     }
 }
