2c16715b4a83f671fdd2da6804c5d440a0875841
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-12 15:46:00 +0300

    Add files via upload

--- MicrosoftTask1.java
+++ MicrosoftTask1.java
@@ -13,16 +13,16 @@ public class MicrosoftTask1 extends Methods {
         int[] input = {-4, -4, 2, 8};
         int max = Integer.MIN_VALUE;
         StringBuilder resStb = new StringBuilder();
-        int res = 0;
+        int res = 0, old = 0;
 
         for (int i : input) {
             for (int l = 0, k = 0; l < input.length - 1; l++, k++) {
-                res = Math.abs(i) * Math.abs(input[l] * Math.abs(input[l + 1]));
+                if (i != input[l]) res = Math.abs(i) * Math.abs(input[l] * Math.abs(input[l + 1]));
                 if (res > max) {
                     max = res;
+                    resStb.delete(0, resStb.length());
                     resStb.append(i).append(input[l]).append(input[l + 1]);
                 }
-
             }
         }
         System.out.printf("%d -> %s ", max, resStb);
