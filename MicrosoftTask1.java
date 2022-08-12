2e27b78abdb3da94a1889655d8683d9c5301a321
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-12 17:21:46 +0300

    Add files via upload

--- MicrosoftTask1.java
+++ MicrosoftTask1.java
@@ -4,8 +4,10 @@ import training.Methods;
 
 public class MicrosoftTask1 extends Methods {
     public static void main(String[] args) {
+        timeAndData();
 
         /**
+         @Web: https://coderpro.com/
          @problem: You are given an array of integers.
          Return the largest product that can be made by multiplying any 3 integers in the array.
          Example:
@@ -19,11 +21,27 @@ public class MicrosoftTask1 extends Methods {
 
         for (int i : input) {                   // Ред: 22 -> изключва текущият елемент от сметката.
             for (int l = 0; l < input.length - 1; l++) {
-                if (i != input[l]) res = Math.abs(i) * Math.abs(input[l] * Math.abs(input[l + 1]));
-                if (res > max) {
-                    max = res;
-                    resStb.delete(0, resStb.length());
-                    resStb.append(i).append(input[l]).append(input[l + 1]);
+                if (i != input[l]) {
+                    res = Math.abs(i) * Math.abs(input[l] * Math.abs(input[l + 1]));
+                    if (res > max) {
+                        max = res;
+                        resStb.delete(0, resStb.length());
+                        resStb.append(i).append(input[l]).append(input[l + 1]);
+                    }
+                }
+            }
+        }
+        System.out.printf("%s = %d ", resStb, max);
+
+        for (int i = 0; i < input.length; i++) {
+            for (int l = 0; l < input.length - 1; l++) {
+                if (i != l) {
+                    res = Math.abs(input[i]) * Math.abs(input[l] * Math.abs(input[l + 1]));
+                    if (res > max) {
+                        max = res;
+                        resStb.delete(0, resStb.length());
+                        resStb.append(input[i]).append(input[l]).append(input[l + 1]);
+                    }
                 }
             }
         }
