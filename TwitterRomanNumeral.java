2d9c45ae1452216d89948c656bc4e286c1ede929
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-21 19:56:16 +0300

    Add files via upload

---
+++ TwitterRomanNumeral.java
@@ -0,0 +1,36 @@
+package DailyInterView;
+
+public class TwitterRomanNumeral {
+    /**
+     * @Problem: Given a Roman numeral, find the corresponding decimal value. Inputs will be between 1 and 3999.
+     *
+     * Example:
+     * Input: IX
+     * Output: 9
+     *
+     * Input: VII
+     * Output: 7
+     *
+     * Input: MCMIV
+     * Output: 1904
+     *
+     * Roman numerals are based on the following symbols:
+     * I     1
+     * IV    4
+     * V     5
+     * IX    9
+     * X     10
+     * XL    40
+     * L     50
+     * XC    90
+     * C     100
+     * CD    400
+     * D     500
+     * CM    900
+     * M     1000
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
