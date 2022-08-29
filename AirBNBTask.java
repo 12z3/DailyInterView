82f2dd3b4bc3e52a65e899d38f665e15e6b971ab
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-29 10:27:37 +0300

    Add files via upload

---
+++ AirBNBTask.java
@@ -0,0 +1,61 @@
+package DailyInterView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AirBNBTask {
+    /**
+     * @problem: This problem was recently asked by AirBNB:
+     * Given a list of words, group the words that are anagrams of each other.
+     * (An anagram are words made up of the same letters).
+     * Example:
+     * Input: ['abc', 'bcd', 'cba', 'cbd', 'efg']
+     * Output: [['abc', 'cba'], ['bcd', 'cbd'], ['efg']]
+     */
+    public static void main(String[] args) {
+        List<String> input = new ArrayList<>(List.of("abc", "bcd", "cba", "cbd", "efg"));
+
+//        StringBuilder word3 = new StringBuilder("abc");
+//        StringBuilder word4 = new StringBuilder("cba");
+
+        groupAnagramWords(input);
+    }
+
+    private static void groupAnagramWords(List<String> input) {
+        List<StringBuilder> res = new ArrayList<>();
+        List<StringBuilder> negativRes = new ArrayList<>();
+
+        StringBuilder word1 = new StringBuilder();
+        StringBuilder word2 = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        StringBuilder negativResult = new StringBuilder();
+
+        for (int i = 0; i < input.size(); i++) {
+            word1.delete(0, word1.length());
+            word1.append(input.get(i));
+            for (int j = i + 1; j < input.size(); j++) {
+                word2.delete(0, word2.length());
+                word2.append(input.get(j));
+                if (stbComparator(word1, word2)) {
+                    result.append(word1).append("-").append(word2).append(" ");
+                } else {
+                    negativResult.append(word1).append("-").append(word2).append(" ");
+                }
+            }
+        }
+        System.out.printf("%s / %s %n", result, negativResult);
+    }
+
+    private static boolean stbComparator(StringBuilder stb1, StringBuilder stb2) {
+        int countChars = 0;
+        if (stb1.length() != stb2.length()) return false;
+        for (int i = 0; i < stb1.length(); i++) {
+            char a = stb1.charAt(i);
+            for (int j = 0; j < stb2.length(); j++) {
+                char b = stb2.charAt(j);
+                if (a == b) countChars++;
+            }
+            if (countChars >= 3) return true;
+        }
+        return false;
+    }
+}
