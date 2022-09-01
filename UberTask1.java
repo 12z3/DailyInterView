1ac99d3f235d0ed776a12c10c36012bf4a04a9b4
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-01 18:24:06 +0300

    Add files via upload

---
+++ UberTask1.java
@@ -0,0 +1,33 @@
+package DailyInterView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UberTask1 {
+    /**
+     * @Problem: This problem was recently asked by Uber:
+     * Given a list of numbers, find if there exists a pythagorean triplet in that list.
+     * A pythagorean triplet is 3 variables a, b, c where a2 + b2 = c2
+     * <p>
+     * Input: [3, 5, 12, 5, 13]
+     * Output: True
+     * Here, 52 + 122 = 132.
+     */
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>(List.of(3, 5, 12, 5, 13));
+        pythagorean(list);
+    }
+
+    private static void pythagorean(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size() - 1; j++) {
+                if (i != j) {
+                    if (Math.pow(list.get(i), 2) == Math.pow(list.get(j), 2) + Math.pow(list.get(j + 1), 2)) {
+                        System.out.printf("%dˆ2 = %dˆ2 + %dˆ2%n", list.get(i), list.get(j), list.get(j + 1));
+                    }
+                }
+            }
+        }
+    }
+}
